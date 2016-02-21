9e8dedf132ecefc30b1cc6666a96142c846698f1
aniolv <aniol.valera8@gmail.com>
2016-02-21 20:02:32 +0100

    Third week assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,30 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
 makeCacheMatrix <- function(x = matrix()) {
-
+        returned <- NULL
+		#returned value will be the variable that will contain the returning object
+        setmatrix <- function(assigned) {
+                x <<- assigned
+                returned <<- NULL
+        }
+        getmatrix <- function() x
+        setsolved <- function(inverse) returned <<- inverse
+        getsolved<- function() returned
+        listm<-list(setmatrix = setmatrix, getmatrix = getmatrix,
+             setsolved = setsolved,
+             getsolved = getsolved)
+		#listm as a list with all the functions to use in this makeCachematrix.This functions
+		#will set the values of the matrix and get them, and the same with the solved matrix.
 }
 
-
-## Write a short comment describing this function
-
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        #we look at the cache to see if the amtrix is already there. This will be that the position in the list is not empty
+        returned <- x$getsolved()
+        if(!is.null(returned)) {
+                message("getting cached data")
+                return(returned)
+        }
+		#if the value is not in the cache function, return will become the slved version of the matrix that is new
+        datam <- x$getmatrix()
+        returned<-solve(datam)
+        x$setsolved(returned)
+        returned
 }
